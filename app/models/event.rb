023ad54b47c6046e6b4ae246a39db2053dad9aeb
bilel <bilel@bilel-iMac.(none)>
2014-08-21 15:08:34 +0100

    add paperclip to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
   attr_accessible :content, :event_date, :title
+  attr_accessible :picture
+  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/images/:style/missing.png"
+  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140821130225) do
+ActiveRecord::Schema.define(:version => 20140821133303) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "event_date"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
 end

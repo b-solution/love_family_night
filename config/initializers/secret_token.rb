d2847669c26836ae25a6030aceeb96e7579e262e
bilel <bilel@bilel-iMac.(none)>
2014-08-21 12:08:51 +0100

    init project lfn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoveFamilyNight::Application.config.secret_token = 'c1b0b3521b5aba5f8a6dfe0e39313e7aefa3c37a17559074172bcdfd4f87748a5b41ea95511e542778618abc7f7ed9d26e77cdb9efd5263c734e6c0100bf38b8'
